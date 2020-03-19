b8f13134425c118549543f1287df5bf1fd4ec6d0
Steve <ycsteven.tsao@gmail.com>
2020-03-19 15:45:16 +0800

    抽出重複的程式碼

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store';
+import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators';
 
 class TodoList extends Component {
     constructor(props) {
@@ -38,28 +39,22 @@ class TodoList extends Component {
             </div>
         );
     }
-    changeInputValue(e) {
-        const action = {
-            type: 'changeInput',
-            value: e.target.value
-        };
-        store.dispatch(action);
-    }
-
     storeChange() {
         this.setState(store.getState());
     }
 
+    changeInputValue(e) {
+        const action = changeInputAction(e.target.value);
+        store.dispatch(action);
+    }
+
     clickBtn() {
-        const action = { type: 'addItem' };
+        const action = addItemAction();
         store.dispatch(action);
     }
 
     deleteItem(index) {
-        const action = {
-            type: 'deleteItem',
-            index
-        };
+        const action = deleteItemAction(index);
         store.dispatch(action);
     }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,4 @@
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './actionTypes';
 const defaultState = {
     inputValue: '寫些東西吧！',
     list: ['吃飯', '睡覺', '再吃飯', '再睡覺', '又吃飯', '又睡覺', '繼續吃飯', '繼續睡覺']
@@ -5,20 +6,20 @@ const defaultState = {
 export default (state = defaultState, action) => {
     // console.log(state, action);
     // Reducer裡只能接收 state，不能改變 state
-    if (action.type === 'changeInput') {
+    if (action.type === CHANGE_INPUT) {
         let newState = JSON.parse(JSON.stringify(state));
         newState.inputValue = action.value;
         return newState;
     }
 
-    if (action.type === 'addItem') {
+    if (action.type === ADD_ITEM) {
         let newState = JSON.parse(JSON.stringify(state));
         newState.list.push(newState.inputValue);
         newState.inputValue = '';
         return newState;
     }
 
-    if (action.type === 'deleteItem') {
+    if (action.type === DELETE_ITEM) {
         let newState = JSON.parse(JSON.stringify(state));
         newState.list.splice(action.index, 1);
         return newState;
